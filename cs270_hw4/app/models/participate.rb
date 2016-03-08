0926c59b9bed56dcde1861b719559cf2a74e3a62
jenneera <jph221@gmail.com>
2016-03-08 07:22:23 +0000

    remove

--- cs270_hw4/app/models/participate.rb
+++
@@ -1,4 +0,0 @@
-class Participate < ActiveRecord::Base
-    has_and_belongs_to_many :event
-    has_and_belongs_to_many :user
-end
--- cs270_hw4/app/models/visit.rb
+++
@@ -1,4 +0,0 @@
-class Visit < ActiveRecord::Base
-   belongs_to :location
-   belongs_to :user
-end
